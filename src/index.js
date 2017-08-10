a5e597715cdbee5264325545ce97b68542993219
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2017-08-10 11:19:22 +0300

    SystemMessage feature added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import './assets/style.css';
 
-// import './example'; // only develop
+import './example'; // only develop
 
 /* import components */
 import MessageBox from './MessageBox/MessageBox';
 import ChatItem from './ChatItem/ChatItem';
+import SystemMessage from './SystemMessage/SystemMessage';
 
 
 export {
 	MessageBox,
 	ChatItem,
+	SystemMessage,
 };
