744e891fbda98bd7366691f7ea25cc53827fda2a
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2017-08-11 18:00:32 +0300

    MessageList and ChatList classNames added example changed

--- example/App.js
+++ example/App.js
@@ -10,32 +10,67 @@ import {
 const loremIpsum = require('lorem-ipsum');
 
 export class App extends Component {
-	random() {
-		return {
-			position: (parseInt(Math.random() * 10 % 2) && 'right'),
-			type: (parseInt(Math.random() * 10 % 2) ? 'file' : 'photo'),
-			text: loremIpsum({count: 1, units: 'sentences'}),
-			data: {
-				uri: 'https://user-images.githubusercontent.com/15075759/29005175-1d023bea-7ade-11e7-86d5-21f87225ef56.png',
-				status: {
-					click: true,
-					loading: 1,
-				}
-			},
-			date: new Date(),
-		};
+
+	token() {
+		return (parseInt(Math.random() * 10 % 3));
+	}
+
+	random(type) {
+		switch (type) {
+			case 'message':
+				var type = this.token();
+				if (type < 1)
+					type = 'photo';
+				else if (type > 1)
+					type = 'file';
+				else
+					type = 'text';
+
+				return {
+					position: (this.token() >= 1 ? 'right' : 'left'),
+					type: type,
+					text: loremIpsum({count: 1, units: 'sentences'}),
+					data: {
+						uri: 'https://user-images.githubusercontent.com/15075759/29005175-1d023bea-7ade-11e7-86d5-21f87225ef56.png',
+						status: {
+							click: false,
+							loading: 0,
+						}
+					},
+					date: new Date(),
+				};
+			case 'chat':
+				return {
+					avatar: 'https://avatars0.githubusercontent.com/u/15075759',
+					alt: loremIpsum({count: 2, units: 'words'}),
+					title: loremIpsum({count: 2, units: 'words'}),
+					date: new Date(),
+					subtitle: loremIpsum({count: 1, units: 'sentences'}),
+					unread: parseInt(Math.random() * 10 % 3),
+				};
+		}
+	}
+
+	test(arr) {
 	}
 
 	render() {
-		var arr = [1,2,3,4,5,6,7,8,9,0];
-		var dataSource = arr.map(x => this.random());
+		var arr = [];
+		for (var i = 0; i < 50; i++)
+			arr.push(i);
+		
+		var messageSource = arr.map(x => this.random('message'));
+		var chatSource = arr.map(x => this.random('chat'));
 
 		return (
-			<div>
-				{
-					<MessageList
-						dataSource={dataSource}/>
-				}
+			<div className='container'>
+				<ChatList
+					className='chat-list'
+					dataSource={chatSource}/>
+				<MessageList
+					className='message-list'
+					dataSource={messageSource}/>
+
 				{
 					// arr.map((x, i) => {
 						// var tmp = this.random();
