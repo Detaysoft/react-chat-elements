ef1d3cc1fb31a3baabc0dc9dc77187190b5c4093
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2017-08-11 08:57:56 +0300

    left notch added

--- src/MessageBox/MessageBox.js
+++ src/MessageBox/MessageBox.js
@@ -33,13 +33,30 @@ export class MessageBox extends Component {
 							data={this.props.data}
 							text={this.props.text}/>
 					}
+
 					<div className="rce-mbox-time rce-mbox-right">
 						12:30
 					</div>
 
-					<svg className="rce-mbox-right-notch" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-						<path d="M0 0v20L20 0"/>
-					</svg>
+					{
+						this.props.position === 'right' ?
+							<svg className="rce-mbox-right-notch" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+								<path d="M0 0v20L20 0"/>
+							</svg>
+						:
+							<div>
+								<svg className="rce-mbox-left-notch" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+									<defs>
+										<filter id="filter1" x="0" y="0">
+											<feOffset result="offOut" in="SourceAlpha" dx="-2" dy="-5" />
+											<feGaussianBlur result="blurOut" in="offOut" stdDeviation="3" />
+											<feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
+										</filter>
+									</defs>
+									<path d="M20 0v20L0 0" filter="url(#filter1)"/>
+								</svg>
+							</div>
+					}
 				</div>
 			</div>
 		);
