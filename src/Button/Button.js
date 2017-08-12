0a244605586ef5819eeaa415fad5dfe033e515ce
mlelyakan <mlelyakan@gmail.com>
2017-08-12 16:01:11 +0300

    Button Js Comment Line Removed #9

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -6,9 +6,6 @@ const classNames = require('classnames');
 export class Button extends Component {
 	render() {
 		return (
-			// <div className="rce-container-input">
-			// 	<input type="text" className="rce-input" placeholder={this.props.placeholder} value={this.props.value} />
-			// </div>
 			<a className={classNames('rce-button', this.props.type)}
 				disabled={this.props.disabled}>
 				{this.props.text}</a>
