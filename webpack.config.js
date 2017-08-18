b26ff6cca8cb5ced641a172448c0c231680e3392
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2017-08-18 11:15:22 +0300

    webpack config stable.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,57 @@
+var webpack = require('webpack');
+var path = require('path');
+var nodeExternals = require('webpack-node-externals');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        'react-chat-elements': './src/index'
+    },
     output: {
-        path: __dirname + '/dist',
-        filename: 'index_bundle.js'
+        path: path.join(__dirname, 'dist'),
+        filename: "main.js",
+        libraryTarget: 'commonjs2',
+        library: 'ReactChatElements',
+    },
+    node: {
+        global: false,
+    },
+    stats: {
+        colors: true,
+        warnings: false,
     },
     module: {
         loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-            { test: /\.css$/, loader: "style-loader!css-loader" },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                include: path.join(__dirname, 'src')
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                }),
+                // loader: "style-loader!css-loader",
+            },
         ]
     },
+    externals: [nodeExternals({
+        whitelist: [/\.(?!(?:jsx?|json)$).{1,5}$/i],
+    })],
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.EnvironmentPlugin([
+            "NODE_ENV",
+        ]),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: false,
+            mangle: false,
+            beautify: false,
+            comments: false,
+        }),
+        new ExtractTextPlugin("main.css"),
+    ],
 };
