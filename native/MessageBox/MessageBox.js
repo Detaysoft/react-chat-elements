57042b2d199da2229f718a987155864f80d63d14
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2017-11-12 22:38:43 +0300

    MessageBox cmp has been added.

---
+++ native/MessageBox/MessageBox.js
@@ -0,0 +1,149 @@
+import React, { Component } from 'react';
+import styles from './MessageBoxStyle.js';
+
+import Avatar from '../Avatar/Avatar';
+
+import {
+    View,
+    Text,
+    Image,
+} from 'react-native';
+
+export class MessageBox extends Component {
+
+    render() {
+        var positionCls = [
+            styles.rceMbox,
+            this.props.position === 'right' && styles.rceMboxRight,
+        ];
+        var thatAbsoluteTime = this.props.type !== 'text' && this.props.type !== 'file' && !(this.props.type === 'location' && this.props.text);
+
+        return (
+            <View
+                style={styles.rceContainerMbox}
+                onClick={this.props.onClick}>
+
+                {
+                    this.props.type === 'system' ?
+                        null
+                        :
+                        <View
+                            style={[
+                                positionCls,
+                            ]}>
+                            <View
+                                style={styles.rceMboxBody}>
+                                {
+                                    (this.props.title || this.props.avatar) &&
+                                    <View
+                                        style={[
+                                            styles.rceMboxTitle,
+                                            this.props.type === 'text' && styles.rceMboxTitleClear,
+                                        ]}
+                                        onClick={this.props.onTitleClick}>
+                                        <View
+                                            style={styles.rceMboxTitleAvatar}>
+                                            {
+                                                this.props.avatar &&
+                                                <Avatar
+                                                    size={{
+                                                        width: 30,
+                                                        height: 30,
+                                                    }}
+                                                    src={this.props.avatar}/>
+                                            }
+                                        </View>
+                                        <View>
+                                        {
+                                            this.props.title &&
+                                            <Text
+                                                style={[
+                                                    styles.rceMboxTitleText,
+                                                    this.props.titleColor && { color: this.props.titleColor },
+                                                ]}>{this.props.title}</Text>
+                                        }
+                                        </View>
+                                    </View>
+                                }
+
+                                {
+                                    this.props.type === 'text' &&
+                                    <Text
+                                        style={styles.rceMboxText}>
+                                        {this.props.text}
+                                    </Text>
+                                }
+
+                                <View
+                                    style={[
+                                        styles.rceMboxTime,
+                                        thatAbsoluteTime && styles.rceMboxTimeBlock
+                                    ]}>
+                                    <Text
+                                        style={styles.rceMboxTimeText}>
+                                    {
+                                        this.props.date &&
+                                        !isNaN(this.props.date) &&
+                                        (
+                                            this.props.dateString ||
+                                            moment(this.props.date).fromNow()
+                                        )
+                                    }
+                                    </Text>
+                                    {
+                                        this.props.status &&
+                                        <Text
+                                            style={styles.rceMboxStatus}>
+                                            {this.props.status}
+                                            {
+                                                // this.props.status === 'waiting' &&
+                                                // <MdIosTime />
+                                            }
+
+                                            {
+                                                // this.props.status === 'sent' &&
+                                                // <MdCheck />
+                                            }
+
+                                            {
+                                                // this.props.status === 'received' &&
+                                                // <IoDoneAll />
+                                            }
+
+                                            {
+                                                // this.props.status === 'read' &&
+                                                // <IoDoneAll color='#4FC3F7'/>
+                                            }
+                                        </Text>
+                                    }
+                                </View>
+                            </View>
+                        </View>
+                }
+            </View>
+        );
+    }
+}
+
+MessageBox.defaultProps = {
+    position: 'left',
+    type: 'text',
+    text: '',
+    title: null,
+    titleColor: null,
+    onTitleClick: null,
+    onForwardClick: null,
+    date: new Date(),
+    data: {},
+    onClick: null,
+    onOpen: null,
+    onDownload: null,
+    forwarded: false,
+    status: null,
+    dateString: 'tarih',
+    notch: true,
+    avatar: null,
+    renderAddCmp: null,
+};
+
+export default MessageBox;
