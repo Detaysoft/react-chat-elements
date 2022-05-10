ea353cd16d5e9dbfe2022cb4864e875af79d8ab2
Emre Güdür <emregudur@gmail.com>
2022-05-10 14:04:39 +0300

    forced commit

---
+++ example/index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var react_dom_1 = require("react-dom");
+require("./App.css");
+var App_1 = require("./App");
+// const render = Component => {
+react_dom_1["default"].render(<App_1["default"] />, document.getElementById('app'));
+// };
+// render(App);
