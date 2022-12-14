08d97456e884431ee544fbfacc206d067c78a4aa
Emre Güdür <emre.gudur@detaysoft.com>
2022-12-14 17:46:26 +0300

    progress option check

--- src/VideoMessage/VideoMessage.tsx
+++ src/VideoMessage/VideoMessage.tsx
@@ -15,15 +15,15 @@ const VideoMessage: React.FC<IVideoMessageProps> = props => {
     trailColor: '#aaa',
     trailWidth: 1,
     step: (
-      state: IProgressOptions,
+      data: IProgressOptions,
       circle: {
         path: { setAttribute: (arg0: string, arg1: string) => void }
         value: () => number
         setText: (arg0: string | number) => void
       }
     ) => {
-      circle.path.setAttribute('trail', state?.state?.color)
-      circle.path.setAttribute('trailwidth-width', state?.state?.width)
+      circle.path.setAttribute('trail', (data.state !== undefined && data.state.color) || '')
+      circle.path.setAttribute('trailwidth-width', (data.state !== undefined && data.state.width) || '')
 
       var value = Math.round(circle?.value() * 100)
       if (value === 0) circle?.setText('')
@@ -45,9 +45,9 @@ const VideoMessage: React.FC<IVideoMessageProps> = props => {
         style={{
           ...(props?.data.width &&
             props?.data.height && {
-            width: props.data.width,
-            height: props.data.height,
-          }),
+              width: props.data.width,
+              height: props.data.height,
+            }),
         }}
       >
         {!downloaded && (
