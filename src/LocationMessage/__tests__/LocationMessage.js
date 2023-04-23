a12f5c23dec5a165c1643d0d626820fd6c2cd063
Satoshi Ogura <117020745+o3osatoshi@users.noreply.github.com>
2023-04-23 22:36:10 +0900

    fix LocationMessage

--- src/LocationMessage/__tests__/LocationMessage.js
+++ src/LocationMessage/__tests__/LocationMessage.js
@@ -5,7 +5,7 @@ import LocationMessage from '../LocationMessage'
 
 describe('LocationMessage component', () => {
   it('should render without issues', () => {
-    const component = shallow(<LocationMessage />)
+    const component = shallow(<LocationMessage data={{ latitude: '', longitude: '', staticURL: '' }} />)
     expect(component.length).toBe(1)
     expect(toJson(component)).toMatchSnapshot()
   })
